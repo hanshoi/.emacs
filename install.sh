b401beb44129c5d723592de737756c4d864447a3
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2021-11-23 10:46:09 +0200

    set script directory as working directory

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CURRENT_DIR=`pwd`
+EMACS_DIR="$(dirname "$0")"
 
 if [ -f $HOME/.emacs ]; then
     mv -f $HOME/.emacs $HOME/.backup_emacs  # store old emacs configuration in case it is needed some day
@@ -11,9 +11,9 @@ fi
 echo ""
 
 echo "; refer hemacs for real configuration.
-;Hemacs is installed in ${CURRENT_DIR}.
+;Hemacs is installed in ${EMACS_DIR}.
 
-(add-to-list 'load-path \"${CURRENT_DIR}\")
+(add-to-list 'load-path \"${EMACS_DIR}\")
 (require 'hemacs-init)
 " > ~/.emacs
 
