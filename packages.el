1486c5e64ed765b03e3b330612afe524ecf70557
hanshoi <hanshoi@iki.fi>
2024-03-18 11:44:49 +0200

    added php :(

--- packages.el
+++ packages.el
@@ -20,6 +20,7 @@
 (package! pet)
 
 (package! elixir-ts-mode)
+;;(package! gleam-mode)
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
